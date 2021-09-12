c71e4dd324f0d2361edfa3703014ef97495d3c4f
Matt Sharp <matthew.sharp3@gmail.com>
2021-09-11 21:35:35 -0400

    fixed naming

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get('/newgame/{playername}/')
-def newgame(playername: str, max: int = 100):
-    g = Game(playername=playername, max=max)
+@app.get('/newgame/{player}/')
+def newgame(player: str, max: int = 100):
+    g = Game(player=player, max=max)
     g.save_game()
-    return {'Player': playername, 'Range': f'1 - {max}', 'Message': 'New game created'}
+    return {'Player': player, 'Range': f'1 - {max}', 'Message': 'New game created'}
 
 
-@app.get('/{playername}/{guess}/')
-def game(playername: str, guess: int):
-    save = load_game(playername)
+@app.get('/{player}/{guess}/')
+def guess(player: str, guess: int):
+    save = load_game(player)
     g = Game(save)
     message = g.check_guess(guess)
     g.save_game()
-    return {'Player': playername, 'Correct': g.correct, 'Guesses': len(g.guesses), 'Message': message}
+    return {"Player": player, "Correct": g.correct, "Guesses": len(g.guesses), "Message": message}
 
 
 @app.get('/scores')
@@ -43,7 +43,7 @@ def sort_scores(sort_by: str = 'guesses', order: str = 'desc'):
     return sorted(scores, key=lambda k: k[sort_key], reverse=rev)
 
 
-@app.get('/open_games')
+@app.get('/open-games')
 def get_open_games(sort_by: str = 'player', order: str = 'desc'):
     open_games = get_game_data(completed=False)
 
